c120869d817a3d9cfc90e83963af00b43c566a92
Alfredo Ochoa <alfredo.ochoa@walmart.com>
2019-08-24 16:41:51 -0500

    Added Genders

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,5 +1,23 @@
 import { Component } from '@angular/core';
-
+export const genders = [
+  { link: 'gen_accion', label: 'Acción y Aventura' },
+  { link: 'gen_anime', label: 'Animé y Videojuegos' },
+  { link: 'gen_biograficas', label: 'Biográficas' },
+  { link: 'gen_scifi', label: 'Ciencia Ficción' },
+  { link: 'gen_cineoro', label: 'Cine de Oro' },
+  { link: 'gen_clasicas', label: 'Clásicas' },
+  { link: 'gen_comedia', label: 'Comedia' },
+  { link: 'gen_deportes', label: 'Deportes' },
+  { link: 'gen_docu', label: 'Documentales' },
+  { link: 'gen_drama', label: 'Drama' },
+  { link: 'gen_familiares', label: 'Familiares' },
+  { link: 'gen_historicas', label: 'Históricas' },
+  { link: 'gen_infantiles', label: 'Infantiles' },
+  { link: 'gen_latino', label: 'Latinoamericanas' },
+  { link: 'gen_musica', label: 'Música' },
+  { link: 'gen_romanticas', label: 'Románticas' },
+  { link: 'gen_terror', label: 'Terror y Suspenso' },
+]
 @Component({
   selector: 'admission-test-root',
   templateUrl: './app.component.html',
@@ -8,6 +26,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = "Depayser";
   logo = "assets/logo.svg"
+  showGendersMobile = false;
+  genders = genders
   navigation = [
     { link: 'inicio', label: 'Inicio' },
     { link: 'catalogo', label: 'Catalogo' },
--- apps/frontend/src/app/home/home.component.ts
+++ apps/frontend/src/app/home/home.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { genders } from '../app.component';
 @Component({
   selector: 'admission-test-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  genders = genders;
   constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() { }
 }
